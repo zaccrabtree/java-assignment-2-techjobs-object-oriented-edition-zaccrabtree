ffb6f88b7e17deeb9e430a28e075a1f1ca528ed3
Zac Crabtree <zac.crabtree1@gmail.com>
2023-02-17 14:57:06 -0600

    Completed tasks 1 and 2

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -10,16 +12,32 @@ public class PositionType {
         id = nextId;
         nextId++;
     }
-
+    // TODO: Add a custom toString() method that returns the data stored in 'value'.
     public PositionType(String value) {
         this();
         this.value = value;
     }
 
-    // TODO: Add a custom toString() method that returns the data stored in 'value'.
-
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+    @Override
+    public String toString() {
+        return value;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositionType that = (PositionType) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
     // Getters and Setters:
 
